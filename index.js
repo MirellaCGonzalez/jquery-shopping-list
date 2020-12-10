1a204ce5294c455f5191dc579685bb260dac2aca
Mirella C Gonzalez <mirellac.gonzalez@gmail.com>
2020-12-10 14:50:10 -0800

    Add index.html,index.js, and main.css

---
+++ index.js
@@ -0,0 +1,41 @@
+function shoppingList() {
+  
+    /*add the item to the list*/
+    $('#js-shopping-list-form').submit(function(event){
+      event.preventDefault();
+      
+      const listItem = $('#shopping-list-entry').val();
+      
+     /*if shopping-list-entry is not equal to empty, select list item */ 
+     if (listItem !== "") {
+        $('.shopping-list').append(`<li>
+          <span class="shopping-item">${listItem}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>
+        <li>`);
+        $('#shopping-list-entry').val("");
+    }
+  });
+  
+  /*click on check button and put a line-through the item from list*/
+  /*CSS style shopping-item__checked*/
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('.shopping-item-controls').siblings('.shopping-item').toggleClass('shopping-item__checked');
+  });
+  
+  /*click on delete button and remove the item from list*/
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
+  }
+  
+  $(shoppingList);
+  
+  
